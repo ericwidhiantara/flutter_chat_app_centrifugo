3f32c0ae2ea3d349cd03678d3d6b935aad9fa963
ericwidhiantara <nukenin.konoha@gmail.com>
2023-10-04 14:29:23 +0700

    feature: add dependency injection using get_it

---
+++ lib/dependencies_injection.dart
@@ -0,0 +1,74 @@
+import 'package:get_it/get_it.dart';
+import 'package:tdd_boilerplate/core/core.dart';
+import 'package:tdd_boilerplate/features/features.dart';
+import 'package:tdd_boilerplate/utils/utils.dart';
+
+GetIt sl = GetIt.instance;
+
+Future<void> serviceLocator({
+  bool isUnitTest = false,
+  bool isHiveEnable = true,
+  String prefixBox = '',
+}) async {
+  /// For unit testing only
+  if (isUnitTest) {
+    await sl.reset();
+  }
+  sl.registerSingleton<DioClient>(DioClient(isUnitTest: isUnitTest));
+  _dataSources();
+  _repositories();
+  _useCase();
+  _cubit();
+  if (isHiveEnable) {
+    await _initHiveBoxes(
+      isUnitTest: isUnitTest,
+      prefixBox: prefixBox,
+    );
+  }
+}
+
+Future<void> _initHiveBoxes({
+  bool isUnitTest = false,
+  String prefixBox = '',
+}) async {
+  await MainBoxMixin.initHive(prefixBox);
+  sl.registerSingleton<MainBoxMixin>(MainBoxMixin());
+}
+
+/// Register repositories
+void _repositories() {
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(sl(), sl()),
+  );
+  sl.registerLazySingleton<UsersRepository>(() => UsersRepositoryImpl(sl()));
+}
+
+/// Register dataSources
+void _dataSources() {
+  sl.registerLazySingleton<AuthRemoteDatasource>(
+    () => AuthRemoteDatasourceImpl(sl()),
+  );
+  sl.registerLazySingleton<UsersRemoteDatasource>(
+    () => UsersRemoteDatasourceImpl(sl()),
+  );
+}
+
+void _useCase() {
+  /// Auth
+  sl.registerLazySingleton(() => PostLogin(sl()));
+  sl.registerLazySingleton(() => PostRegister(sl()));
+
+  /// Users
+  sl.registerLazySingleton(() => GetUsers(sl()));
+}
+
+void _cubit() {
+  /// Auth
+  sl.registerFactory(() => RegisterCubit(sl()));
+  sl.registerFactory(() => AuthCubit(sl()));
+
+  /// Users
+  sl.registerFactory(() => UsersCubit(sl()));
+  sl.registerFactory(() => SettingsCubit());
+  sl.registerFactory(() => MainCubit());
+}
