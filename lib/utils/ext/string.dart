4e3270e6174c847eb78189064634b2e927b8aa34
ericwidhiantara <nukenin.konoha@gmail.com>
2023-10-04 12:22:09 +0700

    feature: tdd utils directory

---
+++ lib/utils/ext/string.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:tdd_boilerplate/core/core.dart';
+import 'package:tdd_boilerplate/utils/utils.dart';
+
+extension StringExtension on String {
+  bool isValidEmail() {
+    return RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+    ).hasMatch(this);
+  }
+
+  void toToastError(BuildContext context) {
+    try {
+      final message = isEmpty ? "error" : this;
+
+      //dismiss before show toast
+      dismissAllToast(showAnim: true);
+
+      showToastWidget(
+        Toast(
+          bgColor: Theme.of(context).extension<CustomColor>()!.red,
+          icon: Icons.error,
+          message: message,
+          textColor: Colors.white,
+        ),
+        dismissOtherToast: true,
+        position: ToastPosition.top,
+        duration: const Duration(seconds: 3),
+      );
+    } catch (e, stackTrace) {
+      FirebaseCrashLogger().nonFatalError(error: e, stackTrace: stackTrace);
+      log.e("error $e");
+    }
+  }
+
+  void toToastSuccess(BuildContext context) {
+    try {
+      final message = isEmpty ? "success" : this;
+
+      //dismiss before show toast
+      dismissAllToast(showAnim: true);
+
+      // showToast(msg)
+      showToastWidget(
+        Toast(
+          bgColor: Theme.of(context).extension<CustomColor>()!.green,
+          icon: Icons.check_circle,
+          message: message,
+          textColor: Colors.white,
+        ),
+        dismissOtherToast: true,
+        position: ToastPosition.top,
+        duration: const Duration(seconds: 3),
+      );
+    } catch (e, stackTrace) {
+      FirebaseCrashLogger().nonFatalError(error: e, stackTrace: stackTrace);
+      log.e("$e");
+    }
+  }
+
+  void toToastLoading(BuildContext context) {
+    try {
+      final message = isEmpty ? "loading" : this;
+      //dismiss before show toast
+      dismissAllToast(showAnim: true);
+
+      showToastWidget(
+        Toast(
+          bgColor: Theme.of(context).extension<CustomColor>()!.pink,
+          icon: Icons.info,
+          message: message,
+          textColor: Colors.white,
+        ),
+        dismissOtherToast: true,
+        position: ToastPosition.top,
+        duration: const Duration(seconds: 3),
+      );
+    } catch (e, stackTrace) {
+      FirebaseCrashLogger().nonFatalError(error: e, stackTrace: stackTrace);
+      log.e("$e");
+    }
+  }
+}
