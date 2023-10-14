8c84a9fa7d419f297d6d5877ecd0a4c5ff09c793
ericwidhiantara <nukenin.konoha@gmail.com>
2023-10-14 19:59:00 +0700

    feature: add local datasource user repository

--- lib/features/users/data/repositories/users_repository_impl.dart
+++ lib/features/users/data/repositories/users_repository_impl.dart
@@ -5,8 +5,12 @@ import 'package:tdd_boilerplate/features/users/users.dart';
 class UsersRepositoryImpl implements UsersRepository {
   /// Data Source
   final UsersRemoteDatasource usersRemoteDatasource;
+  final UsersLocalDatasource usersLocalDatasource;
 
-  const UsersRepositoryImpl(this.usersRemoteDatasource);
+  const UsersRepositoryImpl(
+    this.usersRemoteDatasource,
+    this.usersLocalDatasource,
+  );
 
   @override
   Future<Either<Failure, UserListEntity>> users(UsersParams usersParams) async {
@@ -21,5 +25,43 @@ class UsersRepositoryImpl implements UsersRepository {
         return Right(usersResponse.toEntity());
       },
     );
+  }
+
+  @override
+  Future<Either<Failure, List<UserEntity>>> getSavedUsers() async {
+    try {
+      final data = usersLocalDatasource.getAllData();
+
+      return Right(data);
+    } catch (e) {
+      return Left(NoDataFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> addUser(UserEntity user) async {
+    try {
+      return Right(usersLocalDatasource.addData(user));
+    } catch (e) {
+      return Left(NoDataFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> removeUser(UserEntity user) async {
+    try {
+      return Right(usersLocalDatasource.removeData(user));
+    } catch (e) {
+      return Left(NoDataFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> clearUsers() async {
+    try {
+      return Right(usersLocalDatasource.clearUsers());
+    } catch (e) {
+      return Left(NoDataFailure());
+    }
   }
 }
--- lib/features/users/domain/repositories/users_repository.dart
+++ lib/features/users/domain/repositories/users_repository.dart
@@ -4,4 +4,13 @@ import 'package:tdd_boilerplate/features/users/users.dart';
 
 abstract class UsersRepository {
   Future<Either<Failure, UserListEntity>> users(UsersParams usersParams);
+
+  //Local Database Methods
+  Future<Either<Failure, List<UserEntity>>> getSavedUsers();
+
+  Future<Either<Failure, void>> addUser(UserEntity article);
+
+  Future<Either<Failure, void>> removeUser(UserEntity article);
+
+  Future<Either<Failure, void>> clearUsers();
 }
